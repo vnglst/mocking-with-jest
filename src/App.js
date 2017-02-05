e3be57b63f1304f20d76e14602cee1e6adaefb09
Koen van Gilst <koen.vangilst@gmail.com>
2017-02-05 13:11:52 +0100

    remove all graphical elements, render only info

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React, { Component } from "react";
-import "./App.css";
-import request from "./request";
+import React, { Component } from 'react'
+import './App.css'
+import request from './request'
 
 const renderLine = (user, key) => <li key={key}><b>{key}</b>: {user[key]}</li>
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { user: {} };
+  constructor (props) {
+    super(props)
+    this.state = { user: {} }
   }
 
-  componentDidMount() {
-    request("https://api.github.com/users/vnglst").then(data => {
-      this.setState({ user: data.entity });
-    });
+  componentDidMount () {
+    request('https://api.github.com/users/vnglst').then(data => {
+      this.setState({ user: data.entity })
+    })
   }
 
-  render() {
-    const { user } = this.state 
+  render () {
+    const { user } = this.state
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={user.avatar_url} className="App-logo" alt={`Avatar ${user.name}`} />
-          <h2>{user.name}</h2>
-        </div>
-          <ul style={{listStyle: 'none'}}>
-            { Object.keys(user).map(key => renderLine(user, key)) }
-          </ul>
+      <div className='App'>
+        <ul style={{listStyle: 'none'}}>
+          { Object.keys(user).map(key => renderLine(user, key)) }
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
