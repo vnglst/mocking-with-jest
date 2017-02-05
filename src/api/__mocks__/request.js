b7f0348d8539bd7c5e96cb45bf430b58f2695ffc
Koen van Gilst <koen.vangilst@gmail.com>
2017-02-05 16:19:18 +0100

    adds comments & async/await example

--- src/api/__mocks__/request.js
+++ src/api/__mocks__/request.js
@@ -1,10 +1,13 @@
 const fs = require('fs')
 
 const request = (url) => new Promise((resolve, reject) => {
+  // Get userID from supplied url string
   const lastSlash = url.lastIndexOf('/')
   const userID = url.substring(lastSlash + 1)
+  // Load user json data from a file in de subfolder for mock data
   fs.readFile(`./src/api/__mockData__/${userID}.json`, 'utf8', (err, data) => {
     if (err) reject(err)
+    // Parse the data as JSON and put in the key entity (just like the request library does)
     resolve({ entity: JSON.parse(data) })
   })
 })
--- src/api/__tests_/github.test.js
+++ src/api/__tests_/github.test.js
@@ -4,12 +4,21 @@ jest.mock('../request')
 
 const github = require('../github')
 
-describe('#getUser()', () => {
+describe('#getUser() using Promises', () => {
   it('should load user data', () => {
     return github.getUser('vnglst')
     .then(data => {
       expect(data).toBeDefined()
       expect(data.entity.name).toEqual('Koen van Gilst')
     })
+  })
+})
+
+// The exact same test using async/await
+describe('#getUser() using async/await', () => {
+  it('should load user data', async () => {
+    const data = await github.getUser('vnglst')
+    expect(data).toBeDefined()
+    expect(data.entity.name).toEqual('Koen van Gilst')
   })
 })
